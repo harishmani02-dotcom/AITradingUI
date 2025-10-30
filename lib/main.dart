6f062884167b4489c9ab15df4cc8f0a6b457ca2c
harishmani02-dotcom <harishmani02@gmail.com>
2025-10-31 02:15:18 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,10 @@ import 'providers/auth_provider.dart';
 import 'providers/signals_provider.dart';
 import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 
- <void> main() async {
+
+ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Initialize Supabase
@@ -70,6 +70,7 @@ class AuthWrapper extends StatelessWidget {
     }
   }
 }
+
 
 
 
