d2aeb34caa1a9b71119a9bda6b8aee294acef6d2
harishmani02-dotcom <harishmani02@gmail.com>
2025-12-02 03:43:07 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/ai_trading_signals/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ai_trading_signals/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.ai_trading_signals
+package com.FinsparkAIplay.ai_trading_signals
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
