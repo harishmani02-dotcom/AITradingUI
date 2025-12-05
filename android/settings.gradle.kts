3c0b2b7d67dce0a5fc2f0be7ea73ad9cd51c8f42
harishmani02-dotcom <harishmani02@gmail.com>
2025-12-06 01:53:54 +0530

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -4,18 +4,15 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         
-        // CRITICAL FIX: Use the FLUTTER_ROOT path for the local Maven repository
-        // This is a more robust way to find the plugin in CI/CD environments.
-        maven(url = uri("${settings.rootDir}/../${System.getenv("FLUTTER_ROOT")}/packages/flutter_tools/gradle"))
+        // ⭐ FINAL FIX: Use the FLUTTER_ROOT environment variable directly.
+        // This relies on the GitHub Action setting FLUTTER_ROOT to the correct absolute path.
+        maven(url = uri("${System.getenv("FLUTTER_ROOT")}/packages/flutter_tools/gradle"))
     }
 }
 
 plugins {
     id("com.android.application") version "8.1.2" apply false
-    
-    // NOTE: This line is already fixed from the previous typo
     id("com.android.library") version "8.1.2" apply false 
-    
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
