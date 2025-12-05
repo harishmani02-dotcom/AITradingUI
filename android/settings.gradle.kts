d5c99419adcaf806a8158f1969673ec1760084db
harishmani02-dotcom <harishmani02@gmail.com>
2025-12-06 01:39:40 +0530

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -7,10 +7,10 @@ pluginManagement {
 }
 
 plugins {
-    id "com.android.application" version "8.1.2" apply false
-    id "com.android.library" version "8.1.2" apply false
-    id "org.jetbrains.kotlin.android" version "1.9.0" apply false
-    id "dev.flutter.flutter-gradle-plugin" version "1.0.0" apply false
+    id("com.android.application") version "8.1.2" apply false
+    id("com.android.library") version "8.1.2" apply false // Corrected line
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
 
-include ":app"
+include(":app")
