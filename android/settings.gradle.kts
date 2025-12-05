b8b6e5b89e813e8953a5650a642942977a882fdb
harishmani02-dotcom <harishmani02@gmail.com>
2025-12-06 01:43:23 +0530

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -3,12 +3,16 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        
+        // 💡 CRITICAL FIX: Add the Flutter SDK local Maven repository
+        // This tells Gradle where to find the 'dev.flutter.flutter-gradle-plugin'
+        maven(url = uri("${settings.rootDir}/../.android/Flutter/local/artifacts/repo"))
     }
 }
 
 plugins {
     id("com.android.application") version "8.1.2" apply false
-    id("com.android.library") version "8.1.2" apply false // Corrected line
+    id("com.android.library") version "8.1.2") version "8.1.2" apply false // Corrected syntax
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
