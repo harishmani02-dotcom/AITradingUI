95e7a24d6ce19ebd2abcf8bcacc3e44d62d7ecb5
harishmani02-dotcom <harishmani02@gmail.com>
2025-12-07 02:37:13 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,24 +1,41 @@
+buildscript {
+    ext.kotlin_version = '1.8.0'
+    repositories {
+        google()
+        mavenCentral()
+    }
+ 
+    dependencies {
+        classpath 'com.android.tools.build:gradle:8.1.0'
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+    }
+}
+ 
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
 }
-
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
+ 
+rootProject.buildDir = '../build'
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    project.buildDir = "${rootProject.buildDir}/${project.name}"
 }
 subprojects {
-    project.evaluationDependsOn(":app")
+    project.evaluationDependsOn(':app')
+    
+    // Force compileSdk for all subprojects
+    afterEvaluate { project ->
+        if (project.hasProperty("android")) {
+            android {
+                compileSdkVersion 34
+            }
+        }
+    }
 }
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+ 
+tasks.register("clean", Delete) {
+    delete rootProject.buildDir
 }
+ 
