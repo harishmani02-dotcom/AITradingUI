c10be3a217983d4ea8dc780583c964686539d377
harishmani02-dotcom <harishmani02@gmail.com>
2025-12-06 20:09:31 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.3.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")  // Change from 1.9.0 to 1.9.24
     }
 }
 
@@ -20,6 +20,3 @@ allprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
 }
-
-
-
