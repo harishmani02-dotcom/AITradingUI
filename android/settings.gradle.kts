793959d32d51c613dd1a33e19a08e57ac73d346a
harishmani02-dotcom <harishmani02@gmail.com>
2025-12-07 00:27:02 +0530

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,17 +1,24 @@
 pluginManagement {
-    val flutterSdkPath: String = run {
-        val properties = java.util.Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
-        properties.getProperty("flutter.sdk")
-            ?: throw GradleException("Flutter SDK not found. Define location with flutter.sdk in the local.properties file.")
-    }
-
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+    val flutterSdkPath: String by settings
 
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
+    }
+
+    plugins {
+        id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+        id("com.android.application") version "8.1.0" apply false
+        id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    }
+
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.namespace == "dev.flutter") {
+                useModule("io.flutter:flutter-gradle-plugin:1.0.0")
+            }
+        }
     }
 }
 
