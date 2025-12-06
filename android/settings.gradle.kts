ed775c4cfca06535ec20744eaaff95816c4e2a01
harishmani02-dotcom <harishmani02@gmail.com>
2025-12-06 20:20:12 +0530

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,26 +1,19 @@
 pluginManagement {
-    val flutterSdkPath: String = run {
-        val properties = java.util.Properties()
-        val localPropertiesFile = file("local.properties")
-        if (localPropertiesFile.exists()) {
-            properties.load(java.io.FileInputStream(localPropertiesFile))
-        }
-        val path = properties.getProperty("flutter.sdk")
-        requireNotNull(path) { "flutter.sdk not set in local.properties" }
-        path
-    }
-
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
     }
+    
+    plugins {
+        id "org.jetbrains.kotlin.android" version "1.9.24" apply false
+    }
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id "dev.flutter.flutter-plugin-loader" version "1.0.0"
+    id "com.android.application" version "8.3.0" apply false
+    id "org.jetbrains.kotlin.android" version "1.9.24" apply false
 }
 
-include(":app")
+include ":app"
